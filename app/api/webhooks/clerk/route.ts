a65b317b281ae05c5c2ad83043f84330e9330dc4
sultonbek2009 <sultonoskanboyev@gmail.com>
2025-09-11 14:44:28 +0300

    das

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,3 +1,4 @@
+import { db } from "@/lib/db";
 import { verifyWebhook } from "@clerk/nextjs/webhooks";
 import { NextRequest } from "next/server";
 
@@ -5,8 +6,6 @@ export async function POST(req: NextRequest) {
   try {
     const evt = await verifyWebhook(req);
 
-    // Do something with payload
-    // For this guide, log payload to console
     const { id } = evt.data;
     const eventType = evt.type;
     console.log(
@@ -14,6 +13,38 @@ export async function POST(req: NextRequest) {
     );
     console.log("Webhook payload:", evt.data);
 
+    if (eventType === "user.created") {
+      if (!evt.data.username || evt.data.username.trim() === "") {
+        return new Response("Username is required", { status: 400 });
+      }
+      await db.user.create({
+        data: {
+          clerkId: evt.data.id,
+          username: evt.data.username,
+          avatar: evt.data.image_url,
+          fullName: `${evt.data.first_name} ${evt.data.last_name}`,
+          bio: "Bio is not provided",
+        },
+      });
+    }
+    if (eventType === "user.updated") {
+      await db.user.update({
+        where: { clerkId: evt.data.id },
+        data: {
+          clerkId: evt.data.id,
+          username: evt.data.username || "default_username",
+          avatar: evt.data.image_url,
+          fullName: `${evt.data.first_name} ${evt.data.last_name}`,
+          bio: "Bio is not provided",
+        },
+      });
+    }
+
+    if (eventType === "user.deleted") {
+      await db.user.delete({
+        where: { clerkId: evt.data.id },
+      });
+    }
     return new Response("Webhook received", { status: 200 });
   } catch (err) {
     console.error("Error verifying webhook:", err);
